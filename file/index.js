9af52af7e506beb7d30ca422862b52b91ff772b3
sonH <sonhoai272@gmail.com>
2018-01-26 00:49:44 +0700

    FS

--- file/index.js
+++ file/index.js
@@ -1,7 +1,10 @@
 const express = require("express")
+const fs = require("fs")
 
 const app = express()
+app.use(express.static("public"))
 
 const port = process.env.PORT || 3000
+
 
 app.listen(port)
