5b2db74457247b549bcd3ad6f9d625db7f760937
Nguyễn Sơn <sonhoai272@gmail.com>
2018-05-16 18:18:57 +0700

    nodejs buoi2

--- file/index.js
+++ file/index.js
@@ -6,5 +6,13 @@ app.use(express.static("public"))
 
 const port = process.env.PORT || 3000
 
+// fs.readFile("./public/hello.txt", { encoding: 'utf8' }, (err, content) => {
+//     if (err) return console.log(err)
+//     console.log(content)
+// })
+fs.readdir("./public/", (err, files) => {
+    if (err) return console.log(err)
 
+    console.log(files)
+})
 app.listen(port)
